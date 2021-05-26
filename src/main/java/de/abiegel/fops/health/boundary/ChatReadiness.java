4a6af27d3a9253a992736fa82e12ad71e0e7e42a
andrebiegel <andre.biegel@unymira.com>
2021-05-26 23:53:39 +0200

    basic health

---
+++ src/main/java/de/abiegel/fops/health/boundary/ChatReadiness.java
@@ -0,0 +1,18 @@
+package de.abiegel.fops.health.boundary;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+
+@ApplicationScoped
+@Readiness
+public class ChatReadiness  implements HealthCheck{
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.up("all is ready");
+    }
+    
+}
