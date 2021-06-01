c488ad7e815834b0bdcddb20a78e497c5b73710c
andrebiegel <andre.biegel@icloud.com>
2021-06-01 22:51:35 +0200

    alert component added

---
+++ alerting-app-react/src/components/Alert.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Alert({item}) {
+    return (
+        <div>
+            Hallo Alert
+        </div>
+    )
+}
+
+Alert.propTypes = {
+
+}
+
+export default Alert
+
