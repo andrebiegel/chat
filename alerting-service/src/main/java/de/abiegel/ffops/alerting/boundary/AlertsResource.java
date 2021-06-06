305419db46d1efac9bc611e9c35aec376e439c68
andrebiegel <andre.biegel@icloud.com>
2021-06-06 21:40:18 +0200

    switchign to use hashmap

--- alerting-service/src/main/java/de/abiegel/ffops/alerting/boundary/AlertsResource.java
+++ alerting-service/src/main/java/de/abiegel/ffops/alerting/boundary/AlertsResource.java
@@ -4,6 +4,8 @@ import java.time.Instant;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
@@ -12,6 +14,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -21,29 +24,37 @@ import de.abiegel.ffops.alerting.entity.Attendee;
 @Path("/alerts")
 public class AlertsResource {
 
-    List<Alert> alerts = new ArrayList<>();
+    HashMap<String, Alert> alerts = new HashMap<>();
 
     @PostConstruct
     public void init() {
-        this.alerts.add(Alert.of(UUID.randomUUID().toString(), "B3", " Street 1 City", "BE aware of special thing",
-                Instant.now(), Arrays.asList(new Attendee("test@example.com", " Display Name"))));
-        this.alerts.add(Alert.of(UUID.randomUUID().toString(), "B2", " Street 1 City", "BE aware of special thing",
-                Instant.now(), Arrays.asList(new Attendee("test@example.com", " Display Name"))));
-        this.alerts.add(Alert.of(UUID.randomUUID().toString(), "TH2", " Street 1 City", "BE aware of special thing",
-                Instant.now(), Arrays.asList(new Attendee("test@example.com", " Display Name"))));
+
+        Alert tmp = Alert.of(UUID.randomUUID().toString(), "B3", " Street 1 City", "BE aware of special thing",
+                Instant.now(),
+                new ArrayList<Attendee>(Arrays.asList(new Attendee("test@example.com", " Display Name"))));
+        this.alerts.put(tmp.getId(), tmp);
+        Alert tmp2 = Alert.of(UUID.randomUUID().toString(), "B2", " Street 1 City", "BE aware of special thing",
+                Instant.now(),
+                new ArrayList<Attendee>(Arrays.asList(new Attendee("test@example.com", " Display Name"))));
+        this.alerts.put(tmp2.getId(), tmp2);
+        Alert tmp3 = Alert.of(UUID.randomUUID().toString(), "TH2", " Street 1 City", "BE aware of special thing",
+                Instant.now(),
+                new ArrayList<Attendee>(Arrays.asList(new Attendee("test@example.com", " Display Name"))));
+        this.alerts.put(tmp3.getId(), tmp3);
+
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Alert> alerts() {
-        return this.alerts;
+    public Collection<Alert> alerts() {
+        return this.alerts.values();
     }
 
     @PUT
-    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{id}/attendee")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Alert update(Alert input) {
-        return input;
+    public void addAttendee(@PathParam("id") String alertId, Attendee input) {
+        this.alerts.get(alertId).getAttendees().add(input);
     }
 
 }
