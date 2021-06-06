3cfb58127cc8e4591bf884abcf703225b702cfec
andrebiegel <andre.biegel@icloud.com>
2021-06-06 22:20:22 +0200

    moved css import to app

--- chat-app-react/src/components/App.js
+++ chat-app-react/src/components/App.js
@@ -1,29 +1,31 @@
-import PropTypes from 'prop-types'
-import useLocalStorage from '../hooks/useLocalStorage'
-import '../App.css';
-import OpenConversation from './OpenConversation'
-import WebSocketProvider from '../contexts/WebSocketProvider';
-import {ConversationsProvider, useConversations} from '../contexts/ConversationsProvider'
-import Login from './Login'
-function App({chatroom}) {
-  const [id, setId] = useLocalStorage('id', null)
+import PropTypes from "prop-types";
+import useLocalStorage from "../hooks/useLocalStorage";
+import "../App.css";
+import OpenConversation from "./OpenConversation";
+import WebSocketProvider from "../contexts/WebSocketProvider";
+import {
+  ConversationsProvider,
+  useConversations,
+} from "../contexts/ConversationsProvider";
+import Login from "./Login";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App({ chatroom }) {
+  const [id, setId] = useLocalStorage("id", null);
   const dashboard = (
     <WebSocketProvider chatroom={chatroom} id={id}>
-        <ConversationsProvider chatroomId={chatroom} id={id}>
-          <OpenConversation chatroomId={chatroom} id={id} />
-        </ConversationsProvider>
+      <ConversationsProvider chatroomId={chatroom} id={id}>
+        <OpenConversation chatroomId={chatroom} id={id} />
+      </ConversationsProvider>
     </WebSocketProvider>
-  )
-  return (
-    id ? dashboard : <Login onIdSubmit={setId} />
-  )
+  );
+  return id ? dashboard : <Login onIdSubmit={setId} />;
 }
 
 App.propTypes = {
-  chatroom: PropTypes.string
-}
+  chatroom: PropTypes.string,
+};
 App.defaultProps = {
-  chatroom: 'DefaultRoom'
-}
+  chatroom: "DefaultRoom",
+};
 
 export default App;
